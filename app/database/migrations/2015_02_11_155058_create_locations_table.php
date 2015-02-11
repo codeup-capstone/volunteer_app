bed35dfcbe962155ac472f921f1acffd4578e4f8
Jeff <newuser@News-MacBook-Pro.local>
2015-02-11 15:34:46 -0600

    Completed initial table create migrations

---
+++ app/database/migrations/2015_02_11_155058_create_locations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocationsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('locations', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name', 100);
+			$table->string('address', 100);
+			$table->string('city', 45);
+			$table->char('state',2);
+			$table->char('zip',5);
+			$table->integer('agency_id')->unsigned();
+			$table->foreign('agency_id')->references('id')->on('agencies');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('locations');
+	}
+
+}
---
+++ app/database/migrations/2015_02_11_155648_create_events_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('events', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name', 100);
+			$table->text('description')->nullable();
+			$table->date('event_date');
+			$table->time('start_time');
+			$table->time('end_time');
+			$table->datetime('cutoff_time');
+			$table->string('image_name', 100);
+			$table->string('headline', 100)->nullable();
+			$table->string('image_url', 100)->nullable();
+			$table->string('contact_name', 100);
+			$table->char('contact_phone', 10);
+			$table->string('contact_email', 100);
+			$table->tinyInteger('max_volunteers');
+			$table->integer('agency_id')->unsigned();
+			$table->integer('location_id')->unsigned();
+			$table->foreign('agency_id')->references('id')->on('agencies');
+			$table->foreign('location_id')->references('id')->on('locations');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('events');
+	}
+
+}
