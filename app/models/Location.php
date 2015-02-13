d702f171b75224daa080d8921b87e28d05b05586
Jeff <nomadjeff@gmail.com>
2015-02-12 21:07:11 -0600

    Completed first attempt at models.  We may need to tweak later after further testing.

---
+++ app/models/Location.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Auth\UserTrait;
+use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\Reminders\RemindableTrait;
+use Illuminate\Auth\Reminders\RemindableInterface;
+
+class Location extends BaseModel {
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'locations';
+
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+
+	public function activities()
+	{
+	    return $this->hasMany('Activity');
+	}
+	public function agencies()
+	{
+	    return $this->belongsTo('Agency');
+	}
+
+}
