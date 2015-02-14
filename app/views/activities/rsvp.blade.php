80aab9ab8d5c5a00c73800f692641483eca7af9b
Paul Love <paulqlove@gmail.com>
2015-02-14 14:11:19 -0600

    made a bootstrapp breaktrhough

--- app/views/activities/rsvp.blade.php
+++ app/views/activities/rsvp.blade.php
@@ -17,84 +17,92 @@
       <!-- .row .sixteen-gutter --> 
       <div class="row sixteen-gutter">
       
-        <div class="form">
+        <div class="col-md-9 ">
           
             
    	         <div class="row">
              	
-	             	<div class="col-md-3 embed-responsive embed-responsive-4by3">
-	             		<iframe class="embed-responsive-item" src="/img/agency/habitat.jpg" ></iframe>
+	             	<div class="col-md-4 embed-responsive-4by3">
+	             		<img src="/img/agency/habitat.jpg">
 	   	            </div><!--div for logo -->
    	            	
    	            		
 			        <div class="half-row">
-		             	<div class="col-md-3  equal-height-title column-inner text-center">
+		             	<div class="col-md-4  equal-height-title column-inner text-center">
 		             		<h3>DATE</h3>
-		             	</div>
+		             	</div><!-- date section-->
 		             		
-		             	<div class="col-md-3 equal-height-title column-inner text-center">
+		             	<div class="col-md-4 equal-height-title column-inner text-center">
 		             		<h3>Hosted By</h3>
-		             	</div>
-			        </div>     
+		             	</div><!-- hosted section-->
+			        </div>  <!-- top half row section-->   
+         			
          			<div class="half-row">
-		             	<div class="col-md-6 equal-height-text column-inner text-center">
+		             	<div class="col-md-8 equal-height-text column-inner text-center">
 		             		<h3>Event Name</h3>
-		             	</div>          
-         			</div>
+		             	</div> <!-- event section-->     
+		             		
+         			</div><!-- bottem half row section-->
 		       
+	   	     </div><!-- top third of  section-->
+
+	   	     <div class="row">
+	   	     	<div class="col-md-4 embed-responsive-4by3">
+	             		<img src="/img/agency/habitat.jpg">
+	   	            </div><!--div for logo -->
 	   	     </div>
-
-
 	             
           
-          	<div class="col-md-4">
+          	<div class="col-md-2">
 	             	<div>
 	             		<p>123 Falcon St<br>San Antonio, TX<br>75344</p><!--table data will go here with some blade sytax -->
 	             	</div>
-          	</div>
+          	</div> <!-- address section-->
 	             
-        
+        </div><!-- primary columnsection-->
 
         <!--/.col-x-x-->
-        
         <hr class="visible-xs vertical-spacer vertical-spacer-xs">
         <!-- add some vertical spacing when stacked -->
-        
-	        <div class="col-sm-5 col-md-4">
-	          <div class="column-inner column-featured">
-	            <h3 class="h2 column-featured-headline">RSVP</h3>
-	            <p> <a class="btn btn-featured btn-lg" href="{{{ action('HomeController@showRsvp') }}}"><i class="fa fa-arrow-right fa-fw"></i></a> </p>      
-	          </div>
-	          <!--/.column-inner .column-featured-->
-	        </div><!-- rsvp button -->
-	        <!--/.col-x-x--> 
+        <div class="col-md-3" id="sidebar">
+        	<div class="blog-single-sidebar">
+		        <aside class="col-md-12 ">
+		          <div class="column-inner column-featured">
+		            <h3 class="h2 column-featured-headline">RSVP</h3>
+		            <p> <a class="btn btn-featured btn-md" href="{{{ action('HomeController@showRsvp') }}}"><i class="fa fa-arrow-right fa-fw"></i></a> </p>      
+		          </div>
+		          <!--/.column-inner .column-featured-->
+		        </aside><!-- rsvp button -->
+		        
 
-	        <div class="col-sm-5 col-md-4">
-	          <div class="column-inner column-featured">
-	            <h3 class="h2 column-featured-headline">PASS</h3>
-	         	
-	            <p> <a class="btn btn-featured btn-lg" href="#"><i class="fa fa-arrow-right fa-fw"></i></a> </p>
-	                    
-	          </div>
-	          <!--/.column-inner .column-featured-->
-	        </div><!-- pass button-->
-	        <!--/.col-x-x--> 
-	        <div class="col-sm-5 col-md-4">
-	          <div class="column-inner column-featured">
-	            <h3 class="h2 column-featured-headline">HOME</h3>
-	         	<p> <a class="btn btn-featured btn-lg" href="{{{ action('HomeController@showHome') }}}"> <i class="fa fa-arrow-right fa-fw"></i></a> </p>      
-	          </div>
-	          <!--/.column-inner .column-featured-->
-	        </div> <!-- back to home button -->
-        <!--/.col-x-x--> 
+		        <aside class="col-md-12 ">
+		          <div class="column-inner column-featured">
+		            <h3 class="h2 column-featured-headline">PASS</h3>
+		         	
+		            <p> <a class="btn btn-featured btn-md" href="#"><i class="fa fa-arrow-right fa-fw"></i></a> </p>
+		                    
+		          </div>
+		          <!--/.column-inner .column-featured-->
+		        </aside><!-- pass button-->
+		        
+		        <aside class="col-md-12">
+		          <div class="column-inner column-featured">
+		            <h3 class="h2 column-featured-headline">HOME</h3>
+		         	<p> <a class="btn btn-featured btn-md" href="{{{ action('HomeController@showHome') }}}"> <i class="fa fa-arrow-right fa-fw"></i></a> </p>      
+		          </div>
+		          <!--/.column-inner .column-featured-->
+		        </aside> <!-- back to home button -->
+        	</div><!-- single sidebar section-->
+	        
+	     </div><!-- id sidebar section-->
 
       </div><!-- row that contains rsvp and all data -->
           
-   
-    </div>
+    
       <!-- VERTICAL SPACING -->
       <hr class="vertical-spacer vertical-spacer-lg"> 
     <!-- / .container -->
+   
   </div>
   <!-- /.page-content role=main --> 
   <!--=========== END CONTENT FOR THE PAGE ============================================================ -->
