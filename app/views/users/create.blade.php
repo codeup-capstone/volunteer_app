c32c6ab7de6baeda5388a80c6e76bf1629a64e1a
David Runnels <david.runnels@gmail.com>
2015-02-14 13:36:40 -0600

    Updated user views.

--- app/views/users/create.blade.php
+++ app/views/users/create.blade.php
@@ -1,1 +1,13 @@
-/vagrant/sites/capstone.dev/app/views/users/create.blade.php
+@extends('layouts.master')
+
+@section('content')
+<div class="col-md-3">
+</div>
+<div class="col-md-6">
+    <h4 class="text-center clear"><br>Wecome to Serve Search</h4>
+        
+          <p class="text-center clear">Great opportunities are waitng for you. <a class="link-underline login-toggle" href="#" data-slide="slide" data-target="#login-panel">Login</a></p>
+</div>
+
+
+@stop
