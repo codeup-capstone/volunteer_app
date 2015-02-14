15241e073104f10ad731e6f96aee57d41c592029
David Runnels <david.runnels@gmail.com>
2015-02-14 14:09:19 -0600

    added links to log in and edit

--- app/views/users/create.blade.php
+++ app/views/users/create.blade.php
@@ -4,9 +4,10 @@
 <div class="col-md-3">
 </div>
 <div class="col-md-6">
-    <h4 class="text-center clear"><br>Wecome to Serve Search</h4>
+    <h4 class="text-center clear"><br>Welcome to Serve Search!</h4>
         
-          <p class="text-center clear">Great opportunities are waitng for you. <a class="link-underline login-toggle" href="#" data-slide="slide" data-target="#login-panel">Login</a></p>
+          <p class="text-center clear">Update your profile. <a class="link-underline login-toggle" href="{{{ action('UsersController@create') }}}" data-slide="slide" data-target="#login-panel">Edit </a></p>
+          <p class="text-center clear">Great opportunities are waitng for you. <a class="link-underline login-toggle" href="{{{ action('UsersController@create') }}}" data-slide="slide" data-target="#login-panel">Login</a></p>
 </div>
 
 
