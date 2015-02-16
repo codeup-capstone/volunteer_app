eda2ce0809297287a124c4b61cea1e013fba4975
Paul Love <paulqlove@gmail.com>
2015-02-16 16:47:54 -0600

    touched up landing page

--- app/views/landing.blade.php
+++ app/views/landing.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <!-- =============== BEGIN #page ==============================-->
-<div id="page">
+<div id="page" class="landingBackground">
   <!-- =========== BEGIN CONTENT FOR THE PAGE =========================================================================== -->
   <div class="page-content no-page-gutters" role="main">
     <div class="row ">
@@ -14,7 +14,7 @@
       
       <!-- .row .sixteen-gutter --> 
       
-        <div class="wrapper">
+        <div class=" hidden-md hidden-lg wrapper">
             <img src="bedifferent/theme/assets/images/bird_hand.jpeg" class="img-responsive" alt="Responsive image">
              
         
@@ -25,9 +25,10 @@
          
           <!--/.col-x-x-->
           
+          <hr class="visible-xs vertical-spacer vertical-spacer-xs">
+          <!-- add some vertical spacing when stacked -->
           
-          
-          <div class="col-xs-12 col-sm-12 col-md-4">
+          <div class="col-xs-12 col-sm-12 col-md-4 col-md-offset-9">
             <div class="column-inner column-featured">
               <h3 class="h2 column-featured-headline">Quick Connect</h3>
            	
@@ -39,7 +40,7 @@
           </div>
           <!--/.col-x-x--> 
 
-             <div class="col-xs-12 col-sm-12 col-md-4">
+             <div class="col-xs-12 col-sm-12 col-md-4 col-md-offset-9">
             <div class="column-inner column-featured">
               <h3 class="h2 column-featured-headline">Events</h3>
            	
@@ -50,7 +51,7 @@
             <!--/.column-inner .column-featured-->
           </div>
           <!--/.col-x-x--> 
-             <div class=" col-xs-12 col-sm-12 col-md-4">
+             <div class=" col-xs-12 col-sm-12 col-md-4 col-md-offset-9">
             <div class="column-inner column-featured">
               <h3 class="h2 column-featured-headline">Non Profits</h3>
            	
@@ -65,10 +66,62 @@
         </div>
         <!--/.row sixteen-gutter-->     
    </div>
-      
+      <!-- VERTICAL SPACING -->
+      <hr class="vertical-spacer vertical-spacer-lg">
      
      </div> <!--wrapper -->
-      
+        <div class="hidden-xs hidden-sm">
+
+           <div class="col-xs-12 col-md-12 container-fluid">
+       
+           <div class="row no-gutters" >
+           
+            
+             <!--/.col-x-x-->
+             
+             <hr class="visible-xs vertical-spacer vertical-spacer-xs">
+             <!-- add some vertical spacing when stacked -->
+             
+             <div class="col-xs-12 col-sm-12 col-md-4 col-md-offset-9">
+               <div class="column-inner column-featured">
+                 <h3 class="h2 column-featured-headline">Quick Connect</h3>
+                
+                 <p> <a class="btn btn-featured btn-lg" href="{{{ action('HomeController@showRsvp') }}}">See an Event<i class="fa fa-arrow-right fa-fw"></i></a> </p>
+              
+                 
+               </div>
+               <!--/.column-inner .column-featured-->
+             </div>
+             <!--/.col-x-x--> 
+
+                <div class="col-xs-12 col-sm-12 col-md-4 col-md-offset-9">
+               <div class="column-inner column-featured">
+                 <h3 class="h2 column-featured-headline">Events</h3>
+                
+                 <p> <a class="btn btn-featured btn-lg" href="{{{ action('ActivitiesController@index') }}}">Browse Events<i class="fa fa-arrow-right fa-fw"></i></a> </p>
+              
+                 
+               </div>
+               <!--/.column-inner .column-featured-->
+             </div>
+             <!--/.col-x-x--> 
+                <div class=" col-xs-12 col-sm-12 col-md-4 col-md-offset-9">
+               <div class="column-inner column-featured">
+                 <h3 class="h2 column-featured-headline">Non Profits</h3>
+                
+                 <p> <a class="btn btn-featured btn-lg" href="{{{ action('HomeController@showlistOrg') }}}">Browse Non Profits <i class="fa fa-arrow-right fa-fw"></i></a> </p>
+              
+                 
+               </div>
+               <!--/.column-inner .column-featured-->
+             </div>
+             <!--/.col-x-x--> 
+
+           </div>
+           <!--/.row sixteen-gutter-->     
+      </div>
+     </div><!-- hidden -->   
+
     </div>
     <!-- / .container -->
   </div>
