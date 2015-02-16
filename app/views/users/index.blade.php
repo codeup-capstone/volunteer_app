aef28890b2779dc00bdad697c8c5d242d792e7c0
David Runnels <david.runnels@gmail.com>
2015-02-16 13:47:39 -0600

    updated index and show user blades

--- app/views/users/index.blade.php
+++ app/views/users/index.blade.php
@@ -2,6 +2,36 @@
 
 @section('content')
 
+<div class='main-container'>
+	<div class='posts' id='posts'>
+		<h2>
+			<img class="ls-bg" src="{{{ $user->img_url }}}" alt="">
+			<strong>
+				{{{ $user->first_name }}} &nbsp {{{ $user->first_name }}}
+			</strong>
+		</h2>
+
+			<div class='create-update'>
+				<p>
+					user updated [{{{ $user->updated_at->diffForHumans() }}}]&nbsp
+					joined [{{{ $user->created_at->diffForHumans() }}}]
+				</p>
+			</div>
+
+			<p><h3>{{{ $user->description }}}</h3></p>
+
+			<a href="{{{ action('UsersController@show', $post->id) }}}"><button class='btn btn-primary' id='display-button'>Display User</button></a>
+			<a href="{{{ action('UsersController@edit', $post->id) }}}"><button class='btn btn-info' id='edit-button'>Edit User</button><br></a>
+	</div>
+</div>
+
+@empty
+{{{ "Search not found on the site." }}}
+@endforelse
+
+<div class='text-center'>
+	{{ $users->appends(array('search' => Input::get('search')))->links()}}
+</div>
 
 
 
--- app/views/users/show.blade.php
+++ app/views/users/show.blade.php
@@ -2,7 +2,33 @@
 
 @section('content')
 
+<div>
+	<div class='main-container'>
+		<div class='users' id='users'>
+			<h2>{{{ $user->first_name }}} &nbsp {{{ $user->last_name }}}</h2>
+			<div class='create-update'>
+				profile updated [{{{ $user->updated_at->diffForHumans() }}}]&nbsp
+				joined [{{{ $user->created_at->diffForHumans() }}}]</p>
+			</div>
+				<h5>posted by: {{{ $post->user->email }}}</h5>
+			<p><h3>{{{ $post->body }}}</h3></p>
+			@if (Auth::check())
+		
+			<a href="{{{ action('PostsController@edit', $post->id) }}}"><button class='btn btn-info' id='edit-button'>Edit Post</button></a>
 
+			{{ Form::open(array('action' => array('PostsController@destroy', $post->id), 'method' => 'delete')) }}
+				{{ Form::submit('Delete Post', array('class' => 'btn btn-danger', 'id' => 'delete-button')) }}
+			{{ Form::close() }}
+
+			</div>
+</div>
+	@endif
+
+	@if (Auth::guest())
+		<h2>Welcome Guest!</h2>
+	@endif
+</div>
+	
 
 
 @stop
