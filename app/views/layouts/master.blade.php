5c7e937c739847ea9f45748adb5b6615016313b1
David Runnels <david.runnels@gmail.com>
2015-02-16 08:09:52 -0600

    added successMessage

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -48,6 +48,11 @@
 				<div class="alert alert-danger">{{{ Session::get('errorMessage')}}}</div>
 
 			@endif
+      
+      @if(Session::has('successMessage'))
+        <div class="alert alert-success">{{{ Session::get('successMessage')}}}</div>
+
+      @endif
 		
 			<!-- +++++ No Script Fallback notice +++++ -->
 <noscript>
