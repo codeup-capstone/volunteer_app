7555282e591c53964fd80469e60d68b94dc6f3fa
Jeff <nomadjeff@gmail.com>
2015-02-19 18:56:58 -0600

    Modified Activities.show to display Agency logo dynamically (was hard coded before).

--- app/views/activities/show.blade.php
+++ app/views/activities/show.blade.php
@@ -23,9 +23,9 @@
 		   	         <div class="row no-gutters" id="sidebar">
 		             	
 			             	<div class=" embed-responsive-4by3">
-			             		<img src="/img/agency/habitat.jpg">
+			             		<img src="/img/agency/{{ $activity->agency->image_name }}">
 			   	            </div><!--div for logo -->
-		            	    
+		            	    <br>
 		        	     	<div class="hidden-xs hidden-sm embed-responsive-4by3">
 		                  		<img src="/img/agency/map.png" height="203px" width="259px">
 		        	        </div><!--div for logo -->
